29ff5748ff89237a2b1ca82a6da2703437ac9b7d
espandy <espandy@sina.ccom>
2021-08-25 13:20:51 +0800

    1.master test1

--- nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-consumer-example/src/main/java/com/alibaba/nacos/example/spring/cloud/NacosConsumerApplication.java
+++ nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-consumer-example/src/main/java/com/alibaba/nacos/example/spring/cloud/NacosConsumerApplication.java
@@ -1,29 +1,33 @@
 package com.alibaba.nacos.example.spring.cloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author xiaojing
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class NacosConsumerApplication {
 
-    @LoadBalanced
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+    @Value("${server.port}")
+    private Integer port;
+//    @LoadBalanced
+//    @Bean
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
+    @Autowired
+    private EchoService echoService;
 
     public static void main(String[] args) {
         SpringApplication.run(NacosConsumerApplication.class, args);
@@ -32,14 +36,21 @@ public class NacosConsumerApplication {
     @RestController
     public class TestController {
 
-        private final RestTemplate restTemplate;
+//        private final RestTemplate restTemplate;
+//
+//        @Autowired
+//        public TestController(RestTemplate restTemplate) {this.restTemplate = restTemplate;}
+//
+//        @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
+//        public String echo(@PathVariable String str) {
+//            return restTemplate.getForObject("http://service-provider/echo/" + "consumer port 8080 "+ str , String.class);
+//        }
 
-        @Autowired
-        public TestController(RestTemplate restTemplate) {this.restTemplate = restTemplate;}
 
-        @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
-        public String echo(@PathVariable String str) {
-            return restTemplate.getForObject("http://service-provider/echo/" + str, String.class);
+        @RequestMapping(value = "/user/{str}", method = RequestMethod.GET)
+        public String echo(@PathVariable String str){
+            return  echoService.echo(str+"--->"+"fe-consumer:"+port +" user--->" );
         }
+
     }
 }
